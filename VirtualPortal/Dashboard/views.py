2a4e6a7cd121c0781bc92f7d8b62d1e45c830dfe
Waynekaila <waynekaila1@gmail.com>
2025-10-17 13:32:24 +0100

    Mise à jour-2

--- VirtualPortal/Dashboard/views.py
+++ VirtualPortal/Dashboard/views.py
@@ -6,8 +6,37 @@ from .models import *
 from django.contrib import messages
 from django.core.mail import send_mail
 
+
 # Create your views here.
 @login_required
 def dashboard(request):
-	return render(request, 'Dashboard/student_dashboard.html')
-
+    username = request.user.username
+    
+    if request.user.groups.filter(name='Etudiants').exists():
+        promotion = 'Science de base'
+        NbrCoursR = 14
+        context = {
+            'username': username,
+            'promotion': promotion,
+            'NbrCoursR' : NbrCoursR
+        }
+        return render(request, 'Dashboard/student_dashboard.html', context)
+    elif request.user.groups.filter(name='Enseignants').exists():
+        departement = 'Science de base'
+        NbrCoursE = 4
+        NbrEtudiant = 120
+        NbrDevoir= 5
+        context = {
+            'username': username,
+            'departement' : departement,
+            'NbrCoursE' : NbrCoursE,
+            'NbrEtudiant' : NbrEtudiant,
+            'NbrDevoir' : NbrDevoir
+        }
+        return render(request, 'Dashboard/teacher_dashboard.html', context)
+    else:
+        return redirect('/admin/')
+    
+    
+    
+    
